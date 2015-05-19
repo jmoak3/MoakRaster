718f9f180e42b0acd7b4000565b5ff28406222ba
James Moak <mrjmoak3@gmail.com>
2015-05-18 21:46:34 -0400

    bbox work

---
+++ Material.h~
@@ -0,0 +1,12 @@
+#ifndef MATERIAL_H
+#define MATERIAL_H
+
+typedef struct 
+{
+	float red=0.f;
+	float green=0.f;
+	float blue=0.f;
+	float filler=0.f;
+} Material;
+
+#endif
--- TriangleMesh.c
+++ TriangleMesh.c
@@ -9,14 +9,14 @@ bool DoesIntersectTri(Triangle * tri, Ray * ray, Hit * hit)
 	Vector3 a = tri->mesh->vertPoints[tri->vert[0]];
 	Vector3 b = tri->mesh->vertPoints[tri->vert[1]];
 	Vector3 c = tri->mesh->vertPoints[tri->vert[2]];
-	Vector e1;
+	Vector3 e1;
 	SubVec(&b, &a, &e1);
-	Vector e2;
-	SubVec(&c, &a, &e2);
-	Vector s1; 
-	Cross(&(r.d), &e2, &s1);
+	Vector3 e2;
+	SubVec3(&c, &a, &e2);
+	Vector3 s1; 
+	CrossVec3(&(r.d), &e2, &s1);
 	
-	float denominator = Dot(&s1, &e1);
+	float denominator = DotVec3(&s1, &e1);
 	if (denominator>-0.001f && denominator<0.001f)
 		return false;
 	float invDenom = 1.f/denominator;
@@ -24,17 +24,17 @@ bool DoesIntersectTri(Triangle * tri, Ray * ray, Hit * hit)
 	Vector3 s;
 	SubVec3(&(r.o), &a, &s);
 	
-	float b1 = Dot(&s, &s1)*invDenom;
+	float b1 = DotVec3(&s, &s1)*invDenom;
 	if (b1 < 0.f || b1 > 1.f)
 		return false;
 
-	Vector s2;
-	Cross(&s, &e1, &s2);
-	float b2 = Dot(&(r.d), &s2)*invDenom;
+	Vector3 s2;
+	CrossVec3(&s, &e1, &s2);
+	float b2 = DotVec3(&(r.d), &s2)*invDenom;
 	if (b2 < 0.f || b1+b2 > 1.f)
 		return false;
 	
-	float t = Dot(&e2, &s2)*invDenom;
+	float t = DotVec3(&e2, &s2)*invDenom;
 	if (t < r.min)
 		return false;
 
